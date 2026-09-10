099eff99b14f4a992f797e8aff7f1c179cbc5977
stewiecoolgamerpro23-lab <stewiecoolgamerpro23@gmail.com>
2026-09-10 17:34:40 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,5 +2,5 @@ local gid = game.PlaceId
 
 if gid == 142823291 then
     print("found game: murder mystery 2")
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/stewiecoolgamerpro23-lab/WinterScript/refs/heads/main/games/murder_mystery_2.lua"))()
 end
