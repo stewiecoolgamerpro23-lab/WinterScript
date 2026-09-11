2b838a02117fe09ee2406ce5e52180b3a1606112
stewiecoolgamerpro23-lab <stewiecoolgamerpro23@gmail.com>
2026-09-10 20:01:14 -0500

    Update murder_mystery_2.lua

--- games/murder_mystery_2.lua
+++ games/murder_mystery_2.lua
@@ -1,3 +1,3 @@
 scriptkey = "KEY_HERE"
 
-loadstring(game:HttpGet("https://api.redstoneguard.xyz/api/loader/c76dfc8a-c820-448a-a1d2-aa36b4a1a5bb/init"))()
+loadstring(game:HttpGet("https://api.redstoneguard.xyz/api/loader/02095796-741c-4dfa-831d-3896678a489e/init"))()
